f5650891d53e15605bb066a978c0182b12e91cd8
Andrej Karpathy <andrej.karpathy@gmail.com>
2023-07-25 23:57:03 +0000

    honestly at this point this is a lot more my nanogpt code than llama code

--- model.py
+++ model.py
@@ -1,6 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
-
 import math
 import struct
 import inspect
