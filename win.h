343572675f502656798e1af7ff43f3c1b1594fc7
Aydyn Tairov <den.tairov@gmail.com>
2023-07-27 16:30:22 +0100

    minor whitespaces cleanup

--- win.h
+++ win.h
@@ -13,9 +13,9 @@
  * mman-win32
  */
 
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
-#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
-#endif						
+#ifndef _WIN32_WINNT            // Allow use of features specific to Windows XP or later.
+#define _WIN32_WINNT    0x0501  // Change this to the appropriate value to target other versions of Windows.
+#endif
 
 /* All the headers include this file. */
 #ifndef _MSC_VER
