539dc73196e5f5c0aa8c38c924cb02fef7fadd29
richinseattle <richinseattle@gmail.com>
2023-07-26 22:12:32 -0700

    fix whitespace

--- run.c
+++ run.c
@@ -194,7 +194,8 @@ void matmul(float* xout, float* x, float* w, int n, int d) {
     // W (d,n) @ x (n,) -> xout (d,)
     int i;
     #pragma omp parallel for private(i)
-    for (i = 0; i < d; i++) {        float val = 0.0f;
+    for (i = 0; i < d; i++) {
+        float val = 0.0f;
         for (int j = 0; j < n; j++) {
             val += w[i * n + j] * x[j];
         }
