62053c229da191e3d068aefd7ef29b04e19d731f
aegkmq <140575296+aegkmq@users.noreply.github.com>
2023-07-29 04:15:19 +0900

    Add named positional arguments

--- run.c
+++ run.c
@@ -447,17 +447,6 @@ int argmax(float* v, int n) {
     return max_i;
 }
 
-char *get_arg(int cnt, char *args[], int n) {
-    for (int i = 1; i < n; i++) {
-        if (args[i][0] != '-') {
-            cnt--;
-            if (cnt == 0) {
-                return args[i];
-            }
-        }
-    }
-    return NULL;
-}
 char *get_named_arg(char *arg, char *args[], int n) {
     for (int i = n - 1; i >= 0; i--) { // find the last occurrence
         if (!strncmp(args[i], arg, strlen(arg))) { // if args[i] starts with arg
@@ -466,27 +455,38 @@ char *get_named_arg(char *arg, char *args[], int n) {
     }
     return NULL;
 }
+char *get_arg(int cnt, char *arg, char *args[], int n) {
+    for (int i = 1; i < n; i++) {
+        if (args[i][0] != '-') {
+            cnt--;
+            if (cnt == 0) {
+                return args[i];
+            }
+        }
+    }
+    return arg ? get_named_arg(arg, args, n) : NULL;
+}
 // ----------------------------------------------------------------------------
 
 int main(int argc, char *argv[]) {
 
     // 'checkpoint' is necessary arg
-    char *checkpoint = get_arg(1, argv, argc);  // e.g. out/model.bin
+    char *checkpoint = get_arg(1, "--checkpoint=", argv, argc);  // e.g. out/model.bin
     if (checkpoint == NULL) {
         printf("Usage: %s <checkpoint_file> [temperature] [steps] [prompt]\n", argv[0]);
         return 1;
     }
 
     // optional temperature. 0.0 = (deterministic) argmax sampling. 1.0 = baseline
-    char *temperature_arg = get_arg(2, argv, argc); // e.g. 1.0, or 0.0
+    char *temperature_arg = get_arg(2, "--temperature=", argv, argc); // e.g. 1.0, or 0.0
     float temperature = temperature_arg ? atof(temperature_arg) : 0.9f;
 
     // max number of steps to run for, 0: use seq_len
-    char *steps_arg = get_arg(3, argv, argc);
+    char *steps_arg = get_arg(3, "--steps=", argv, argc);
     int steps = steps_arg ? atoi(steps_arg) : 256;
 
     // prompt string
-    char *prompt = get_arg(4, argv, argc);
+    char *prompt = get_arg(4, "--prompt=", argv, argc);
 
     // by default, seed rng with time. if you want deterministic behavior use temperature 0.0
     char *seed_arg = get_named_arg("--seed=", argv, argc);
