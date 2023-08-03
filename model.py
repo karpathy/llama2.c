3fc07645d6b31f573a5c4a0b1157a1090e6a82f7
rahulschand <changrahul0320@gmail.com>
2023-08-03 19:58:54 +0530

    minor changes to rope & comments

--- model.py
+++ model.py
@@ -201,7 +201,7 @@ class Transformer(nn.Module):
         self.tok_embeddings.weight = self.output.weight # https://paperswithcode.com/method/weight-tying
 
         # some useful precompute for the RoPE relative positional embeddings. TODO why * 2 here? confuse
-        freqs_cis = precompute_freqs_cis(self.params.dim // self.params.n_heads, self.params.max_seq_len * 2)
+        freqs_cis = precompute_freqs_cis(self.params.dim // self.params.n_heads, self.params.max_seq_len)
         self.register_buffer("freqs_cis", freqs_cis, persistent=False)
         
         # init all weights
