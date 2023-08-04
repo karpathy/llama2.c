ac2b435151a644dbdb0544a623febba905623085
Michael Cusack <michael.cusack@color.com>
2023-08-04 16:55:26 +0700

    docs

--- save_model.py
+++ save_model.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 #!/usr/bin/env python
-"""Saves the model as a TorchScript."""
+"""Saves the model as a TorchScript.
+
+The resulting file can be loaded in C++ code and then used for training or infrence with:
+    #include <torch/script.h>
+    torch::jit::Module module = torch::jit::load("model.pt")
+"""
 
 import glob
 import os
