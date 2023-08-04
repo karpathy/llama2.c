f67185958b5c3e3c690422430220e3db755e9628
Michael Cusack <michael.cusack@color.com>
2023-08-04 17:07:41 +0700

    Model args in save script

--- save_model.py
+++ save_model.py
@@ -16,9 +16,18 @@ import torch
 
 from model import ModelArgs, Transformer
 
+
 def main() -> None:
-    model_args = ModelArgs(dim=512, n_layers=6, n_heads=8, vocab_size=32000)
-    model = Transformer(model_args)
+    model = Transformer(
+        ModelArgs(
+            dim=288,
+            n_layers=6,
+            n_heads=6,
+            multiple_of=32,
+            dropout=0.0,
+            vocab_size=32000,
+        )
+    )
     torch.jit.save(torch.jit.script(model), "model.pt")
 
 
