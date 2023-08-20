d9ea173f35f991435147086e6af820abcef6d449
Roger Allen <rallen@gmail.com>
2023-08-19 21:05:11 -0700

    fix warning

--- run.cu
+++ run.cu
@@ -875,7 +875,7 @@ int main(int argc, char *argv[]) {
 
     // default inits
     char *checkpoint_path = NULL;  // e.g. out/model.bin
-    char *tokenizer_path = "tokenizer.bin";
+    char *tokenizer_path = (char *)"tokenizer.bin";
     float temperature = 1.0f; // 0.0 = greedy deterministic. 1.0 = original. don't set higher
     float topp = 0.9f;        // top-p in nucleus sampling. 1.0 = off. 0.9 works well, but slower
     rng_seed = 0; // seed rng with time by default
