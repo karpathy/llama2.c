2439f197352013d6194e287114c17c2848eb9510
Ruhollah Majdoddin <r.majdodin@gmail.com>
2023-08-26 22:32:28 +0000

    kv-cache for Attention.forward()

--- sample.py
+++ sample.py
@@ -5,7 +5,7 @@ import os
 import pickle
 from contextlib import nullcontext
 import torch
-from model import ModelArgs, Transformer, RMSNorm, TransformerBlock
+from model import ModelArgs, Transformer, RMSNorm, TransformerBlock, Attention, apply_rotary_emb, repeat_kv
 from tokenizer import Tokenizer
 
 from tinystories import get_tokenizer_model_path
@@ -45,7 +45,66 @@ def RMSNorm_forward(self, x):
             output = torch.cat((kv_cache[id(self)], output), dim=-2)
             kv_cache[id(self)] = output
         return output
+
+def Attention_forward(
+        self,
+        x: torch.Tensor,
+        freqs_cos: torch.Tensor,
+        freqs_sin: torch.Tensor,
+    ):
+        bsz, seqlen, _ = x.shape
+
+        # QKV
+        if kv_cache.get(id(self)) is not None:
+            x = x[..., -1:, :]
+        xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
+        if kv_cache.get(id(self)) is None:
+            kv_cache[id(self)] = (xq, xk, xv)
+        else:
+            kv_cache[id(self)] = (xq, xk, xv) = (torch.cat((kv_cache[id(self)][0], xq), dim=1),
+                                              torch.cat((kv_cache[id(self)][1], xk), dim=1),
+                                              torch.cat((kv_cache[id(self)][2], xv), dim=1))
+        #fixme assuming causual mask, xq is needed just for last token, doesn't need to be cached
+        #fixme can cashing optimize the following?
+
+        xq = xq.view(bsz, seqlen, self.n_local_heads, self.head_dim)
+        xk = xk.view(bsz, seqlen, self.n_local_kv_heads, self.head_dim)
+        xv = xv.view(bsz, seqlen, self.n_local_kv_heads, self.head_dim)
+
+        # RoPE relative positional embeddings
+        xq, xk = apply_rotary_emb(xq, xk, freqs_cos, freqs_sin)
+
+        # grouped multiquery attention: expand out keys and values
+        xk = repeat_kv(xk, self.n_rep)  # (bs, seqlen, n_local_heads, head_dim)
+        xv = repeat_kv(xv, self.n_rep)  # (bs, seqlen, n_local_heads, head_dim)
+
+        # make heads into a batch dimension
+        xq = xq.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
+        xk = xk.transpose(1, 2)
+        xv = xv.transpose(1, 2)
+
+        # flash implementation
+        if self.flash:
+            output = torch.nn.functional.scaled_dot_product_attention(xq, xk, xv, attn_mask=None, dropout_p=self.dropout if self.training else 0.0, is_causal=True)
+        else:
+            # manual implementation
+            scores = torch.matmul(xq, xk.transpose(2, 3)) / math.sqrt(self.head_dim)
+            assert hasattr(self, 'mask')
+            scores = scores + self.mask[:, :, :seqlen, :seqlen]   # (bs, n_local_heads, seqlen, cache_len + seqlen)
+            scores = F.softmax(scores.float(), dim=-1).type_as(xq)
+            scores = self.attn_dropout(scores)
+            output = torch.matmul(scores, xv)  # (bs, n_local_heads, seqlen, head_dim)
+
+        # restore time as batch dimension and concat heads
+        output = output.transpose(1, 2).contiguous().view(bsz, seqlen, -1)
+
+        # final projection into the residual stream
+        output = self.wo(output)
+        output = self.resid_dropout(output)
+        return output
+
 RMSNorm.forward = RMSNorm_forward
+#Attention.forward = Attention_forward
 
 kv_cache = {}
 
